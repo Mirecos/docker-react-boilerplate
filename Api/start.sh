7453d3c96d768ff9424670de480b63134326b2f3
Mirecos <johndoe@example.com>
2025-07-08 14:54:54 +0200

    Upgrade prisma to latest + remove pnpm + exposte port for prisma studio

--- Api/start.sh
+++ Api/start.sh
@@ -3,10 +3,10 @@ if [ "$PRODUCTION_MODE" = "True" ]; then
     echo "Starting in production mode"
     npx prisma generate
     pnpm run migrate:prod
-    exec pnpm run prod
+    exec npm run prod
 else
     echo "Starting in development mode"
     npx prisma generate
-    pnpm run migrate:dev
-    npx prisma studio & exec pnpm run dev
+    npm run migrate:dev
+    npx prisma studio & exec npm run dev
 fi
--- Frontend/start.sh
+++ Frontend/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ "$PRODUCTION_MODE" = "True" ]; then
     echo "Starting in production mode"
-    exec pnpm serve -s build -l 3000
+    exec npx serve -s build -l 3000
 else
     echo "Starting in development mode"
-    exec pnpm run start
+    exec npm run start
 fi
