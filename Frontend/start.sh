a202cda46dceb693176b07eebdc16ad97d06d5ee
Michel <michelrecchia1@gmail.com>
2025-07-22 13:59:17 +0200

    Optimize images + add prod docker compsoe

--- Frontend/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ "$PRODUCTION_MODE" = "True" ]; then
-    echo "Starting in production mode"
-    exec npx serve -s build -l 3000
-else
-    echo "Starting in development mode"
-    exec npm run start
-fi
