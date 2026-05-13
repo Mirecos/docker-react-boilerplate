a3a3a0a10565bb65f844b69bbc08b5882c37d5ab
Mich <michelrecchia1@gmail.com>
2026-05-13 09:30:13 +0200

    Fix permission update workflow

--- Api/src/authorizations/Actions.ts
+++ Api/src/authorizations/Actions.ts
@@ -31,59 +31,6 @@ const rootUser = {
     }
 }
 
-// Initialization script
-export async function initPermissions(){
-    const permissions: { label: string }[] = []
-    const salt = await bcrypt.genSalt();
-    const hashedRootPassword = await bcrypt.hash(rootUser.password, salt)
-
-    // Create permissions
-    for (const item of Object.values(PermissionLabel)) {
-        permissions.push({ label: item })
-        await prisma.permission.upsert({
-            where: { label: item },
-            update: {},
-            create: { label: item }
-        })
-        logger.info(`Permission ${item} ensured.`)
-    }
-
-    // Create Super-Admin role
-    if(! await roleExists(RoleLabel.SUPERADMIN)){
-        logger.info("Creating role : " + RoleLabel.SUPERADMIN)
-        await prisma.role.create({data: {label: RoleLabel.SUPERADMIN, permissions: {connect: permissions}}})
-    }else {
-        logger.info("Updating role : " + RoleLabel.SUPERADMIN)
-        await prisma.role.update({
-            where: { label: RoleLabel.SUPERADMIN },
-            data: { permissions: { set: permissions.map(p => ({ label: p.label })) } }
-        })
-    }
-
-    // Create Roles
-    for(const item in RoleLabel){
-        if(item === RoleLabel.SUPERADMIN)continue
-        else{
-            if(! await roleExists(item)){
-                logger.info(`Creating role : ${item}`)
-                await prisma.role.create({data: {label: item, permissions: {connect: getPermissions(item as RoleLabel).map(p => ({ label: p }))}}})
-            }else logger.error(`Role ${item} already exists !`)
-        }
-    }
-
-    // Ensure super-user account
-    await prisma.user.upsert({
-        where: { email: rootUser.email },
-        update: { role: { connect: { label: RoleLabel.SUPERADMIN } } },
-        create: { ...rootUser, password: hashedRootPassword }
-    })
-    logger.info("Super-user ensured.")
-
-    // Success
-    logger.info("Permissions, roles, and super-user were successfully initialized !")
-    return 0
-}
-
 export async function updatePermissions(){
     const salt = await bcrypt.genSalt();
     const permissions: { label: string }[] = []
